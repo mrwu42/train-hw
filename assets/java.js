2733e948ec6359576ced5879e5d3e37adb2fb0c5
Wayne Wu <mrwu42@gmail.com>
2020-02-21 18:03:40 -0500

    moment.js not working

--- assets/java.js
+++ assets/java.js
@@ -33,21 +33,21 @@ $("#addTrainBtn").on("click", function(){
     $("#firstTrainInput").val("");
     $("#frequencyInput").val("");
 
+    $("#traintable").append("<tr><td>" + trainName + "</td><td>" + destination +"</td><td>" + frequency + "</td><td>" + firstTrain + "</td></tr>");
     return false;
 })
 
-trainData.ref().on("child_added", function(snapshot){
-    var name = snapshot.val().name;
-    var destination = snapshot.val().destination;
-    var frequency = snapshot.val().frequency;
-    var firstTrain = snapshot.val().firstTrain;
+// trainData.ref().on("child_added", function(snapshot){
+//     var name = snapshot.val().name;
+//     var destination = snapshot.val().destination;
+//     var frequency = snapshot.val().frequency;
+//     var firstTrain = snapshot.val().firstTrain;
 
-    var remainder = moment().diff(moment.unix(firstTrain), "minutes") %frequency;
-    var minutes = frequency - remainder;
-    var arrival = moment().add(minutes, "m").format("hh:mm A");
+//     var remainder = moment().diff(moment.unix(firstTrain), "minutes") %frequency;
+//     var minutes = frequency - remainder;
+//     var arrival = moment().add(minutes, "m").format("hh:mm A");
 
-    console.log(remainder)
+//     console.log(remainder)
 
-    $("trainTable > tBody").append("<tr><td>" + name + "</td><td>" + destination +"</td><td>" + frequency + "</td><td>" + arrival + "</td><td>" + minutes + "</td></tr>");
     
-})
+// })
