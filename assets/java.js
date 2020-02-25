007231a41bbe9602b3bb6d0c8edf1a0a3e457106
Wayne Wu <mrwu42@gmail.com>
2020-02-25 17:21:07 -0500

    final

--- assets/java.js
+++ assets/java.js
@@ -16,9 +16,9 @@ var trainData = firebase.database();
 $("#addTrainBtn").on("click", function(){
     var trainName = $("#trainNameInput").val().trim();
     var destination = $("#destinationInput").val().trim();
-    var firstTrain = moment($("#firstTrainInput").val().trim(), "HH:MM").subtract(10, "years").format("X");
+    var firstTrain = moment($("#firstTrainInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
     var frequency = $("#frequencyInput").val().trim();
-
+    console.log("firsttrain " + firstTrain);
     var newTrain = {
         name: trainName,
         destination: destination,
@@ -33,21 +33,28 @@ $("#addTrainBtn").on("click", function(){
     $("#firstTrainInput").val("");
     $("#frequencyInput").val("");
 
-    $("#traintable").append("<tr><td>" + trainName + "</td><td>" + destination +"</td><td>" + frequency + "</td><td>" + firstTrain + "</td></tr>");
     return false;
 })
 
-// trainData.ref().on("child_added", function(snapshot){
-//     var name = snapshot.val().name;
-//     var destination = snapshot.val().destination;
-//     var frequency = snapshot.val().frequency;
-//     var firstTrain = snapshot.val().firstTrain;
+trainData.ref().on("child_added", function(snapshot){
+    var name = snapshot.val().name;
+    var destination = snapshot.val().destination;
+    var frequency = snapshot.val().frequency;
+    var firstTrain = snapshot.val().firstTrain;
 
-//     var remainder = moment().diff(moment.unix(firstTrain), "minutes") %frequency;
-//     var minutes = frequency - remainder;
-//     var arrival = moment().add(minutes, "m").format("hh:mm A");
+    var remainder = moment().diff(moment.unix(firstTrain), "minutes") %frequency;
+    var minutes = frequency - remainder;
+    var arrival = moment().add(minutes, "m").format("hh:mm");
 
-//     console.log(remainder)
+    console.log(firstTrain);
+    console.log(remainder);
+    console.log(minutes);
+    console.log(arrival);
 
     
-// })
+
+    
+
+    $("#traintable").append("<tr><td>" + name + "</td><td>" + destination +"</td><td>" + frequency + "</td><td>" + arrival + "</td><td>" + minutes + "</td></tr>");
+    
+})
